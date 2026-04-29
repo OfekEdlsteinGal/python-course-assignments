787a8561fb9247cfb7f704b39c66dfdfdd17bb0c
OfekEdlsteinGal <ofekeded@gmail.com>
2026-04-29 21:28:30 +0300

    Create main_gui.py

---
+++ day03/main_gui.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+from gibbs_lib import gibbs_free_energy
+
+def calculate():
+    dh = float(entry_dh.get())
+    t = float(entry_t.get())
+    ds = float(entry_ds.get())
+    result_label.config(text=str(gibbs_free_energy(dh, t, ds)))
+
+root = tk.Tk()
+
+entry_dh = tk.Entry(root)
+entry_dh.pack()
+
+entry_t = tk.Entry(root)
+entry_t.pack()
+
+entry_ds = tk.Entry(root)
+entry_ds.pack()
+
+tk.Button(root, text="Calculate", command=calculate).pack()
+
+result_label = tk.Label(root)
+result_label.pack()
+
+root.mainloop()
