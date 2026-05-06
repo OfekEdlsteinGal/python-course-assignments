ee9420517c8655c76b776be45bc0229a63feb6d2
Ofek Edlstein Gal <ofekeded@gmail.com>
2026-05-06 17:05:40 +0300

    Update main_input and clean gitignore

--- day04/main_input.py
+++ day04/main_input.py
@@ -2,16 +2,17 @@ from gibbs_lib import gibbs_free_energy, interpret_delta_g
 
 # Get input from user
 delta_H = float(input("Enter enthalpy change ΔH (in kJ/mol): "))
-temperature = float(input("Enter temperature (in Kelvin): "))
 delta_S = float(input("Enter entropy change ΔS (in J/mol·K): "))
+temperature = float(input("Enter temperature:" ))
 unit = input("Enter temperature unit (K for Kelvin, C for Celsius): ")
 
 # Calculate using library
-delta_G = gibbs_free_energy(delta_H, temperature,unit, delta_S)
+delta_G = gibbs_free_energy(delta_H, delta_S, temperature, unit)
 
 # Print result
 print("\nResult:")
 print("ΔG =", delta_G, "kJ/mol")
+
 # Interpretation
 print(interpret_delta_g(delta_G))
 
