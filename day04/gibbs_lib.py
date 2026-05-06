3b24afe211da3e4b04a4ba8637f1fff3e6c2c918
Ofek Edlstein Gal <ofekeded@gmail.com>
2026-05-06 17:16:41 +0300

    Update main_input and gibbs_lib

--- day04/gibbs_lib.py
+++ day04/gibbs_lib.py
@@ -25,3 +25,19 @@ def interpret_delta_g(delta_G):
         return "The reaction is non-spontaneous."
     else:
         return "The reaction is at equilibrium."
+
+
+def equilibrium_temperature(delta_H, delta_S):
+    """
+    Calculate equilibrium temperature where ΔG = 0
+    ΔH in kJ/mol
+    ΔS in J/mol·K
+    Returns temperature in Kelvin
+    """
+
+    delta_S_kJ = delta_S / 1000
+
+    if delta_S_kJ == 0:
+        return None
+
+    return delta_H / delta_S_kJ
--- day04/main_input.py
+++ day04/main_input.py
@@ -1,4 +1,4 @@
-from gibbs_lib import gibbs_free_energy, interpret_delta_g
+from gibbs_lib import gibbs_free_energy, interpret_delta_g,equilibrium_temperature
 
 # Get input from user
 delta_H = float(input("Enter enthalpy change ΔH (in kJ/mol): "))
@@ -8,6 +8,7 @@ unit = input("Enter temperature unit (K for Kelvin, C for Celsius): ")
 
 # Calculate using library
 delta_G = gibbs_free_energy(delta_H, delta_S, temperature, unit)
+eq_temp = equilibrium_temperature(delta_H, delta_S)
 
 # Print result
 print("\nResult:")
@@ -15,6 +16,13 @@ print("ΔG =", delta_G, "kJ/mol")
 
 # Interpretation
 print(interpret_delta_g(delta_G))
+
+if eq_temp is not None:
+    print("Equilibrium temperature =", round(eq_temp, 2), "K")
+else:
+    print("Equilibrium temperature cannot be calculated because ΔS = 0")
+
+
 
 
 
